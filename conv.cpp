3a9a92fefddbd58af44ad49aacc60214d705d1d9
ghosthunter15 <ghosthunt15@gmail.com>
2017-05-02 11:41:31 -0400

    added some temp conversions.

--- conv.cpp
+++ conv.cpp
@@ -7,11 +7,15 @@ int main(){
     
   int option;
   float weight;
+  float fah;
+  float cel;
     
   cout << "Weight Convert: " << endl;
   cout << endl;
   cout << "1: lbs to kgs" << endl;
   cout << "2: kgs to lbs" << endl;
+  cout << "3: fah to cel\n"; // may error
+  cout << "4: cel to fah\n"; // may error
   cout << endl;
     
   cout << "Please enter an option: ";
@@ -19,7 +23,9 @@ int main(){
     
   // 1lb = 0.454kg
   // 1kg = 2.205lb
-    
+  // cel = (fah - 32) * 5/9
+  // fah = cel * 9/5 + 32
+
   switch (option){
     
    case 1:
@@ -41,7 +47,28 @@ int main(){
      cout << endl;
      cout << weight << "lbs";
      break;
-        
+	
+   case 3:	//enter temp in fah to get cel
+      cout << "Enter temp in fah: ";
+      //get temp
+      cin >> fah;
+
+      //math and results
+      cel = (fah - 32) * 5/9;
+      cout << endl;
+      cout << cel << "C°\n";
+      break;
+
+   case 4:	// enter temp in cel to get fah
+      cout << "Enter temp in cel: ";
+      cin >> cel;
+
+      //math and results
+      fah = cel * 9/5 + 32;
+      cout << endl;
+      cout << fah << "F°\n";
+      break;
+
    default:
      cout << endl;
      cout << "Please enter a different number.";
